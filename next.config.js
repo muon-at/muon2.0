14f6c4c0b57fe46ed85439e5852543ef5ee1ceff
muon-at <post@atventilasjon.no>
2026-03-03 22:35:21 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
 };
 
-module.exports = nextConfig;
+export default nextConfig;
